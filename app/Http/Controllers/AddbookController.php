31358c399df8bf1122f9daeb56d01a1f5200195c
ShahrulNukman <shahrulnukmans@gmail.com>
2021-01-16 00:50:09 +0800

    update by Shahrul

---
+++ app/Http/Controllers/AddbookController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreaddbooksRequest;
+use App\Http\Requests\UpdateaddbooksRequest;
+use App\Models\addbooks;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
+use Symfony\Component\HttpFoundation\Response;
+
+class AddbookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+    
+        $addbooks = addbooks::all();
+        return view('addbooks.index',compact('addbooks'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('addbooks.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreaddbooksRequest $request)
+    {
+        addbooks::create($request->validated());
+        return redirect()->route('addbooks.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(addbooks $addbooks)
+    {
+        return view('addbooks.show', compact('addbooks'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(addbooks $addbooks)
+    {
+        return view('addbooks.edit', compact('addbooks'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateaddbooksRequest $request, addbooks $addbooks)
+    {
+       $addbooks ->update($request->validated());
+       return redirect()->route('addbooks.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(addbooks $addbooks)
+    {
+        $addbooks->delete();
+        return redirects()->route('addbooks.index');
+    }
+}
