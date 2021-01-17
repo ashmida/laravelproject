14f09ee61134d4d87c052a41a673d9d65be07969
aimienadiaa <nadiayusrin3108@gmail.com>
2021-01-17 17:11:15 +0800

    update by aimie

--- resources/views/addrating/index.blade.php
+++ resources/views/addrating/index.blade.php
@@ -21,7 +21,7 @@
                                         Title
                                     </th >
                                     <th scope="col" class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                        Rating
+                                        Rating/Feedback
                                     </th>
                                     <th scope="col" width="200" class="px-6 py-3 bg-gray-50">
 
